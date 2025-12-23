14265ab01e6f2c13d6f137dc19c0740cd7c42686
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-23 11:30:24 -0400

    study: console.log();

---
+++ javascript_basico/aula03/script.js
@@ -0,0 +1,7 @@
+console.log("estou sendo exibido no console do navegador"); // String = texto 
+console.log('2'); // Number = Número
+console.log(`o console é um objeto e o log é um método dentro de console`); // crase são usadas para fazer template strings
+console.log("eu tenho:", 17, "anos");
+console.log(`meu nome é "luca" 'hubner'`);
+
+console.log(123, 2.23, 123234.231414, "lucas hubner");
