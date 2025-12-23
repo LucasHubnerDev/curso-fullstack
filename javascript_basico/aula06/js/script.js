6fb5c41897b17358fb9325725aa4b6696d3b337d
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-23 12:17:02 -0400

    study: variáveis com let em js

---
+++ javascript_basico/aula06/js/script.js
@@ -0,0 +1,8 @@
+let name = "lucas";
+let surname = "hubner";
+let age = 17;
+let dateBirth = new Date().getFullYear() - age; 
+
+console.log(`meu nome é ${name} ${surname}.`);
+console.log(`eu tenho ${age} anos.`);
+console.log(`nascido em ${dateBirth}`)
