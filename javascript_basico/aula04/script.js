fc71dfc2dfe4cd6755132715bbf3be5282d7482f
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-23 11:44:28 -0400

    study: comentários em js

---
+++ javascript_basico/aula04/script.js
@@ -0,0 +1,9 @@
+// comentários são ignorados pelo motor do Javascript
+console.log("comentários não interfere no fluxo de código")
+// outra forma de comentários
+/** COMENTÀRIOS
+ * comentário 1
+ * comentário 2
+ * comentário 3
+ * comentário 4
+ */
