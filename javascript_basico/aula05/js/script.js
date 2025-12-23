b896e7dc97fc244362f34677ca7f396d277f49b5
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-23 12:02:57 -0400

    study: HTML + JavaScript

---
+++ javascript_basico/aula05/js/script.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+// posso usar o comando node no cmd, para executar diretamente lá o arquivo js.
+
+alert("seja bem vindo") // comando do navegador, não pode ser interpretado pelo node, pois são ambientes de execução diferentes.
