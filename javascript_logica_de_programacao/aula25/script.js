8c5d4b8af2949b872acff20d3e5e7f6f1ef91281
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-27 08:47:22 -0400

    study(js): mais sobre if

---
+++ javascript_logica_de_programacao/aula25/script.js
@@ -0,0 +1,10 @@
+const numero = 5
+
+// na primeira condição verdadeira o javascript vai sair fora da estrutura de condição
+if(numero >= 0 && numero <= 5) {
+    console.log(`10 >= ${numero} >= 0`)
+} else {
+    console.log("condições não compridas")
+}
+
+console.log("independente do resultado da condição o código vai continuar")
