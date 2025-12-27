300bcc813a1234ac436eb544764cea3475cda6f8
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-27 07:50:33 -0400

    study(js): Operadores de comparação

---
+++ javascript_logica_de_programacao/aula21/script.js
@@ -0,0 +1,19 @@
+/** OPERADORES DE COMPARAÇÃO    
+ * > maior
+ * >= maior ou igual
+ * < menor
+ * <= menor ou igual
+ * == igual
+ * === idêntico
+ * != diferente
+ * !== totalmente diferente
+ */
+
+// expressões de comparação me retorna valores booleanos
+console.log(10 > 5) 
+
+const comp = 10 <= 7
+console.log(comp)
+
+console.log(20 == "20") // true
+console.log(20 === "20") // false
