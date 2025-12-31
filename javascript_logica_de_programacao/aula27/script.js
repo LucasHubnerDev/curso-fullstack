dc6bd0108b1277fa7a22299255b1fd17220a441a
LucasHubnerDev <hubner.dev@gmail.com>
2025-12-31 08:03:28 -0400

    study(js): operação ternária

---
+++ javascript_logica_de_programacao/aula27/script.js
@@ -0,0 +1,17 @@
+// Operação Ternária
+// (condição) ? (valor para verdadeiro) : (valor para false)
+
+let cor;
+
+console.log(`${cor ? cor : "cor padrão"}`)
+
+let statu;
+const money = "3000"
+const verificacao = money >= 3000 ? statu = "vip": statu = "normal"; 
+
+console.log(statu)
+
+let valor = 200;
+const valorPadrao = valor || "100"
+
+console.log(valorPadrao)
