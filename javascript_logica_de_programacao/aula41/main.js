5db80a99975a6a4499a0549d864bba6565a20b6a
LucasHubnerDev <hubner.dev@gmail.com>
2026-01-03 10:57:56 -0400

    practice(js): lógica de programação

---
+++ javascript_logica_de_programacao/aula41/main.js
@@ -0,0 +1,4 @@
+const maiorNumero = (...numbers) => Math.max(...numbers)
+
+const teste = maiorNumero(2, 9, -5, -7, 12, -12, 22, -33)
+console.log(`o maior número é ${teste}`)
---
+++ javascript_logica_de_programacao/aula42/main.js
@@ -0,0 +1,6 @@
+const ePaisagem = (x, y) => x > y
+
+const imagem1 = ePaisagem(200, 400)
+const imagem2 = ePaisagem(1920, 1080)
+
+console.log(imagem1, imagem2)
