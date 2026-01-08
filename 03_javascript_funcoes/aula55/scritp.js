e594e6035361552739594aeb6c49d0748d762774
LucasHubnerDev <hubner.dev@gmail.com>
2026-01-08 09:44:32 -0400

    study(js): IIFE

---
+++ 03_javascript_funcoes/aula55/scritp.js
@@ -0,0 +1,10 @@
+// IIFE -> Immediately invoked function expression
+// Funções que se auto executa imediatamente
+(function(idade, peso, altura) {
+    const msg = "hello friend"
+    console.log(msg)
+    console.log(idade, peso, altura)
+})(17, 50, 1.60);
+
+const msg = "your were never a hero, eddie"
+
